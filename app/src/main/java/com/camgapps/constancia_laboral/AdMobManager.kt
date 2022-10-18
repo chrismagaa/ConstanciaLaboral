e77d3cc21b321eff87a4b2409756aaecff7e5d34
Christian Magaña <cmaganapc@corporativosade.com.mx>
2022-10-18 16:22:08 -0500

    new ads

--- app/src/main/java/com/camgapps/constancia_laboral/AdMobManager.kt
+++ app/src/main/java/com/camgapps/constancia_laboral/AdMobManager.kt
@@ -6,8 +6,11 @@ import com.google.android.gms.ads.AdError
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.FullScreenContentCallback
 import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.OnUserEarnedRewardListener
 import com.google.android.gms.ads.interstitial.InterstitialAd
 import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
+import com.google.android.gms.ads.rewarded.RewardedAd
+import com.google.android.gms.ads.rewarded.RewardedAdLoadCallback
 import java.security.PrivateKey
 
 object AdMobManager {
@@ -15,6 +18,8 @@ object AdMobManager {
 
     private var mInterstitialAd: InterstitialAd? = null
     const val idIntersticial = "ca-app-pub-4702215318330979/6730449051"
+    private val TAG = "AdmobManager"
+
 
     fun loadAd(activity: Activity) {
         if (mInterstitialAd == null) {
@@ -60,6 +65,43 @@ object AdMobManager {
             mInterstitialAd?.show(activity)
         }else{
             onShowAd()
+        }
+    }
+
+
+    private var mRewardedAd: RewardedAd? = null
+    private var idRewardedAd = "ca-app-pub-4702215318330979/1186328937"
+
+    fun loadRecompensate(activity: Activity, onPositive: () -> Unit, onCancel: () -> Unit) {
+        var adRequest = AdRequest.Builder().build()
+
+        RewardedAd.load(activity, idRewardedAd, adRequest, object : RewardedAdLoadCallback() {
+            override fun onAdFailedToLoad(adError: LoadAdError) {
+                Log.d(TAG, adError?.toString())
+                mRewardedAd = null
+                onCancel()
+            }
+
+            override fun onAdLoaded(rewardedAd: RewardedAd) {
+                Log.d(TAG, "Ad was loaded.")
+                mRewardedAd = rewardedAd
+                showRecompensate(activity, onPositive, onCancel)
+            }
+        })
+
+    }
+
+
+
+    fun showRecompensate(activity: Activity, onShow: () -> Unit, onCancel: () -> Unit){
+        if (mRewardedAd != null) {
+            mRewardedAd?.show(activity, OnUserEarnedRewardListener() {
+                Log.d(TAG, "User earned the reward.")
+                onShow()
+            })
+        } else {
+            onCancel
+            Log.d(TAG, "The rewarded ad wasn't ready yet.")
         }
     }
 
